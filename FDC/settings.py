c73307e205ce632e8d6753f2ea61c7e543f9ff53
Bakr AbuHassiba <96659482+BakrAbuHassiba@users.noreply.github.com>
2025-08-04 21:02:23 +0300

    Update settings.py

--- FDC/settings.py
+++ FDC/settings.py
@@ -65,9 +65,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'FDC.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -79,8 +76,6 @@ DATABASES = {
     }
 }
 
-# Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -98,9 +93,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -110,13 +102,8 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
@@ -154,6 +141,4 @@ EMAIL_PORT = 465
 EMAIL_USE_SSL = True
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')  # Get from .env
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD') # EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # on cmd do this -> set EMAIL_HOST_PASSWORD=your-secure-password
-# EMAIL_HOST_USER = 'daw@fdenergies.com'
-# EMAIL_HOST_PASSWORD = 'Dona@112021'
-# DEFAULT_FROM_EMAIL = 'daw@fdenergies.com'
+
